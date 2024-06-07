49c0847eaa9385e82cd2d123473e3a6fdcfbd847
oluwasola alufa <solaoluwa@xenonrice.onmicrosoft.com>
2024-06-08 00:27:11 +0100

    06/08/24

---
+++ learnPython/dayOne.py
@@ -0,0 +1,91 @@
+#character_name = "John"
+#character_age =  30
+
+#print ("There once was a man named " + character_name + ", ")
+#print ("he was " + character_age + "years old. ")
+
+#character_name = "Mike"
+#print ("He really liked the name " + character_name + ", ")
+#print ("but didn't like being " + character_age + ".")
+
+
+# working with STRINGS 
+
+#print ("solo Academy")
+
+#print("Solo\nAcademy")
+
+#print("Solo\"Academy")
+
+
+dollar = "Solo Academy"
+#print(dollar)
+#print(dollar + " is cool.") 
+
+
+
+#print(dollar.lower())
+
+#print(dollar.upper())
+
+#print(dollar.upper().islower())
+
+#print(len(dollar))
+
+#print(dollar[2])
+
+#passing a Parameter
+
+
+#print(dollar.index("S"))
+
+#print(dollar.replace("Solo", "Akanji"))
+
+
+
+# working with NUMBERS
+
+#print(3 * 4 + 5)
+
+#print(3 * (4 + 5)) #parenthesis are akknowledged first
+
+#my_num = 5
+#print(str(my_num))
+
+#print(str(my_num) + " my favorite number.")
+
+# FUNCTIONS
+# math functions: colloection of code thatdoes something
+
+# ABS - absolute value
+
+#my_num = -6
+#print(abs(my_num))
+
+# POW - raised to the power of 
+
+#print(pow(14, 6)) # 14^6
+
+# MAX & MIN
+
+#print(max(4, 6))
+
+#print(min(4, 6))
+
+#print(round(3.5))
+
+
+
+# IMPORTING MATH FUNCTIONS
+
+from math import *
+
+
+print(floor(3.5))
+
+print(ceil(3.5))
+
+print(sqrt(36)) 
+
+
+
